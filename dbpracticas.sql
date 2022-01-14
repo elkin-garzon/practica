df0bfb2a7c15f0a198c54ed5f0486f8ab3fa0c9f
elkin-garzon <elkingarzonyaya@gmail.com>
2022-01-13 21:23:46 -0500

    ajust

--- dbpracticas.sql
+++ dbpracticas.sql
@@ -273,6 +273,36 @@ LOCK TABLES `test` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `mail` varchar(255) NOT NULL,
+  `result` bigint(20) NOT NULL,
+  `id_test` bigint(20) NOT NULL,
+  `estado` tinyint(1) NOT NULL DEFAULT '1',
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `id_test` (`id_test`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_test`) REFERENCES `test` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Dumping routines for database 'dbpracticas'
 --
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -285,4 +315,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-13 20:21:39
+-- Dump completed on 2022-01-13 21:21:05
